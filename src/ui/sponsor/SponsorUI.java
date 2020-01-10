08dc10e782029415e08cddc5a1b421b3eb0890da
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2020-01-10 15:41:53 +0100

    commit sponsor part

---
+++ src/ui/sponsor/SponsorUI.java
@@ -0,0 +1,26 @@
+package src.ui.sponsor;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class SponsorUI {
+	
+	private Stage primaryStage;
+	
+	public void start(Stage primaryStage) throws IOException {
+		
+		FXMLLoader root = new FXMLLoader(getClass().getResource("SponsorForm.fxml"));
+		
+		Parent parent = root.load();
+		Scene scene = new Scene(parent);
+		primaryStage.setTitle("SponsorHandler");
+		Button btn = new Button();
+		btn.setLayoutX(100);
+	    btn.setLayoutY(80);
+	}
+}
